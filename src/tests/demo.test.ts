d40fb6458d77d05a09f447efa0fea2adeb622034
Konstantin Bläsi <kblaesi@gmail.com>
2024-11-29 17:19:04 +0100

    update test case

--- src/tests/demo.test.ts
+++ src/tests/demo.test.ts
@@ -1,94 +1,63 @@
 import { zod } from "$lib/adapters/zod.js";
 import { superValidate } from "$lib/superValidate.js";
+import { stringify } from "devalue";
 import { describe, test } from 'vitest';
 import { z } from "zod";
 
+const ZodSchema = z.object({
+    addresses: z.object({
+        additional: z.discriminatedUnion('type', [
+            z.object({
+                type: z.literal('poBox'),
+                name: z.string()
+                    // these constraints cause the test failure
+                    // Validation error instead of schema error would be expected?
+                    .min(1, "min len").max(10, "max len")
+                    .default("")
+            }),
+            z.object({
+                type: z.literal('none'),
+            }),
+        ])
+    })
+})
+const FormSchema = zod(ZodSchema)
+type FormSchema = typeof FormSchema['defaults']
+
+async function validate(data: unknown) {
+    const formInput = new FormData()
+
+    formInput.set('__superform_json', stringify(data));
+    try {
+        await superValidate(formInput, FormSchema)
+    } catch (err) {
+        console.error(err)
+        //
+        throw err
+    }
+}
 
 describe("Demo", () => {
     test("Bad", async () => {
-        const schema = zod(
-            z.object({
-                addresses: z.object({
-                    additional: z.discriminatedUnion('type', [
-                        z.object({
-                            type: z.literal('poBox'),
-                            name: z.string().min(1, 'err min len').max(10, 'err max len')
-                        }),
-                        z.object({
-                            type: z.literal('none'),
-                        }),
-                    ]).default({ type: 'none' })
-                })
-            })
-        )
-
-        const formInput = new FormData()
-        formInput.set(
-            '__superform_json',
-            JSON.stringify([
-                {
-                    addresses: 1
-                },
-                {
-                    additional: 2
-                },
-                {
-                    type: 3,
-                    name: 4
-                },
-                'poBox',
-                ''
-            ])
-        )
-        try {
-            await superValidate(formInput, schema)
-        } catch (err) {
-            console.error(err)
-            //
-            throw err
-        }
+        const data = {
+            addresses: {
+                additional: {
+                    type: 'poBox',
+                    name: ''
+                }
+            }
+        } satisfies FormSchema;
+        await validate(data)
     })
 
     test("Good", async () => {
-        const schema = zod(
-            z.object({
-                addresses: z.object({
-                    additional: z.discriminatedUnion('type', [
-                        z.object({
-                            type: z.literal('poBox'),
-                            name: z.string().min(1, 'err min len').max(10, 'err max len')
-                        }),
-                        z.object({
-                            type: z.literal('none'),
-                        }),
-                    ]).default({ type: 'poBox', name: '' })
-                })
-            })
-        )
-
-        const formInput = new FormData()
-        formInput.set(
-            '__superform_json',
-            JSON.stringify([
-                {
-                    addresses: 1
-                },
-                {
-                    additional: 2
-                },
-                {
-                    type: 3,
-                    name: 4
-                },
-                'poBox',
-                ''
-            ])
-        )
-        try {
-            await superValidate(formInput, schema)
-        } catch (err) {
-            console.error(err)
-            throw err
-        }
+        const data = {
+            addresses: {
+                additional: {
+                    type: 'none'
+                }
+            }
+        } satisfies FormSchema;
+        await validate(data)
     })
 })
